94cc949ec42ac7c98869b928fc4f49dc63e2dc51
Nick <nsardonini@gmail.com>
2020-08-28 09:10:30 -0400

    add static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def index
+    flickr = Flickr.new ENV['flickr_key'], ENV['flickr_secret']
+    @flickr_id = params[:flickr_user_id]
+    @urls = []
+    
+    if @flickr_id
+      @photos = flickr.people.getPhotos(user_id: @flickr_id)
+
+      @photos.each do |photo|
+        photo_info = flickr.photos.getInfo(photo_id: photo.id)
+        @urls << Flickr.url_n(photo_info)
+      end
+    end
+  end
+end
