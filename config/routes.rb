1222cf9e4188afc1c49c4042221bae9c91388383
Nick <nsardonini@gmail.com>
2020-08-28 09:09:25 -0400

    add root page & route for index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :static_pages, only: :index
+
+  root 'static_pages#index'
 end
